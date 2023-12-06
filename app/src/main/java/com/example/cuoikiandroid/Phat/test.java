25cec0330b6c2de0333d7e486c2cf1a68b66595e
anhkhoaIT <super.khoa14@gmail.com>
2023-12-06 14:06:01 +0700

    #

---
+++ app/src/main/java/com/example/cuoikiandroid/Phat/test.java
@@ -0,0 +1,10 @@
+package com.example.cuoikiandroid.Phat;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.cuoikiandroid.databinding.ActivityMainBinding;
+
+public class test {
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+}
