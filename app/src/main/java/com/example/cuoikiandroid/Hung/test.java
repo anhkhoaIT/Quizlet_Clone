9053849dfa21e4eb3bcde95bd8b790b74cbfe7ac
anhkhoaIT <super.khoa14@gmail.com>
2023-12-06 14:07:29 +0700

    #

---
+++ app/src/main/java/com/example/cuoikiandroid/Hung/test.java
@@ -0,0 +1,10 @@
+package com.example.cuoikiandroid.Hung;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.cuoikiandroid.databinding.ActivityMainBinding;
+
+public class test {
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+}
